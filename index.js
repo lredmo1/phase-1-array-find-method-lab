1e4acdf8b10e54c284b906f19fa149c52aeb5206
Lisa <redlisa125@gmail.com>
2021-09-22 07:55:37 -1000

    commit tree 331c64b0fe7da04a13db1fdcc42e59b41e8f38ea

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+function superbowlWin(array) {
+    let foundPerson = array.find(isWin);
+    if (foundPerson) {
+        return foundPerson.year;
+    } else {
+        return foundPerson;
+    }
+}
+
+function isWin(resultObject) {
+    return resultObject.result === "W";
+}
+
